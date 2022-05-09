aeb6700b54406de1e30d66c239a46b43dcff9850
MirzaAyon <ayonmirza98@gmail.com>
2022-05-10 00:53:41 +0600

    boolean added

--- variable.js
+++ variable.js
@@ -1,6 +1,18 @@
+// 2.
 /****** Variable / 5 things in a variable*****/
 var price = 21;
 var age = 13;
 var temperature = 37;
 
-console.log(price, age, temperature);
+//console.log(price, age, temperature);
+
+// 3. type string with single/double quatation
+
+var name = "ayon plabon";
+var name2 = 'ayon plabon mirza';
+
+// Boolean
+var pass = true;
+var eat = false;
+
+
