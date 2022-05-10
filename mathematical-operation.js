5740dde883d5364a9457c49d3fd16357020fb495
MirzaAyon <ayonmirza98@gmail.com>
2022-05-10 08:30:32 +0600

    mathematical operation added

---
+++ mathematical-operation.js
@@ -0,0 +1,15 @@
+var onionPrice = 43;
+var eggPrice = 10;
+var eggQuantity = 7;
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+var priceDifference = onionPrice - eggPrice;
+// console.log(priceDifference);
+var eggPriceMultiplication = eggPrice * eggQuantity;
+// console.log(eggPriceMultiplication);
+
+var moneyAvailable = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyAvailable / orangePrice;
+console.log(orangeQuantity);
+
