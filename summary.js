f7a8ccc4e76cf108e68a1da730d1c4c823faee51
MirzaAyon <ayonmirza98@gmail.com>
2022-05-10 20:19:53 +0600

    modulas added

---
+++ summary.js
@@ -0,0 +1,20 @@
+//number
+var eggPrice = 30;
+eggPrice = 30 * 2;
+console.log(eggPrice);
+console.log(typeof eggPrice);
+
+//string
+var singer = 'Avril Lavigne';
+var singer = "Avril Lavigne";
+
+//boolean 
+var isHot = true;
+
+//Modular
+var mangoes = 7;
+var hungryPerson = 2;
+var remaining = mangoes / hungryPerson;
+console.log(remaining);
+var remaining2 = mangoes % hungryPerson;
+console.log(remaining2);
