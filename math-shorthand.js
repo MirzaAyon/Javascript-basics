991ac5c2b6e6132703ebae78c1dae2f59a1a48ad
MirzaAyon <ayonmirza98@gmail.com>
2022-05-10 09:07:22 +0600

    mathematical shorthand added

---
+++ math-shorthand.js
@@ -0,0 +1,37 @@
+var price1 = 31;
+var price2 = 10;
+var sum = price1 + price2;
+var difference = price2 - price1;
+var mul = price1 * price2;
+var div = price1 / price2;
+// console.log(sum);
+// console.log(difference);
+// console.log(mul);
+// console.log(div);
+
+var doublePrice = price2 * 20;
+var newPrice = price1 + 10;
+// console.log(newPrice);
+
+//price1 = price1 + 30;
+price1 += 44; //It is called shorthand
+// console.log(price1);
+//age price1 variable declare kora ache tai notun kore price1 variable declare kora jabe na,sudhu new maan set korlei hbe 
+// price2 = price2 - 2;
+price2 -= 2;
+// console.log(price2);
+
+var age = 14;
+// age = age + 1;
+// age += 1;
+// age++;
+//sudhu 1 kore barar jonno ++ use hoe
+
+console.log(age);
+
+var love = 100;
+// love = love - 1;
+// love -= 1;
+love--;
+console.log(love);
+//3 ta condition e rakhle 97 hbe
