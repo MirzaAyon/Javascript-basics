a3f4613a9d40c8876e8c2ca3f04037bde3858cf2
MirzaAyon <ayonmirza98@gmail.com>
2022-07-03 09:47:16 +0600

    done

---
+++ quiz.js
@@ -0,0 +1,4 @@
+// 1 . What is Javascript? (Just google: wikipedia javascript and read first four paragraphs) - ans - google
+//4 . Which company developed JavaScript? (feel free to google it) - ans -  netscape
+
+//13 -  valid variable types - undefined,string,boolean
